62007db119cef3bf0b47088040b7a22f9b7f3808
Walter Junior <75254804+w4lterjr@users.noreply.github.com>
2025-10-07 14:39:38 +0000

    bolsa_2

--- components/Bolsa.js
+++ components/Bolsa.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import styles from "../styles/Cambio.module.css";
+import styles from "../styles/Bolsa.module.css";
 
 const App = () => {
   const [rates, setRates] = useState({
